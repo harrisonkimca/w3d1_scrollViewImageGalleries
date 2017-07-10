e03d6ee7d63ecd0db6a525f7824f950cc08f9495
hkimca <harrison.kim.ca@gmail.com>
2017-07-10 14:51:24 -0400

    Initial Commit

---
+++ 2w3d1_scrollViewImageGalleries/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2w3d1_scrollViewImageGalleries
+//
+//  Created by Seantastic31 on 10/07/2017.
+//  Copyright © 2017 Seantastic31. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
